22a9086353aa0befb4ecb298fd786e116a5f820c
Joey Kelly <joey@joeykelly.net>
2016-03-09 20:20:34 -0600

    Create slackware-bootstrap-perl-and-python.pl
    This needs work, I know. I should do something more intelligent with /etc/slackpkg/mirrors.
    
    Anyway, if you have a fresh install, this is the first script you need to run, since Ansible depends upon Python and my scripts depend upon Perl. The slackware-installsets.yaml playbook offers to install the rest of the d set, but chicken comes before egg today.

---
+++ slackware-bootstrap-perl-and-python.pl
@@ -0,0 +1,47 @@
+#!/usr/bin/perl -w
+
+# ansible_slackware_bootstrap
+#
+# File: slackware-install-perl-and-python.pl
+# Version 1.0
+# Copyright Joey Kelly (joeykelly.net)
+# March 9, 2016
+# License: GPL version 2
+#
+# https://github.com/mmlj4/ansible_slackware_bootstrap
+
+
+# It seems we can't do much of anything remotely unless Perl and python are installed on the target slackware box, so let's get them installed.
+
+use strict;
+
+my $host = shift || '';
+$host = safechars($host);
+
+my $platformbits = shift || '';
+$platformbits = safechars($platformbits);
+die unless ($platformbits == 32 || $platformbits == 64);
+
+# # ___FIMXE___ use uname -m and determine this programmatically
+# choose your poison... but verify these links yourself
+my %mirror;
+$mirror{32} = 'http://slackware.cs.utah.edu/pub/slackware/slackware-14.1/';
+$mirror{64} = 'http://slackware.cs.utah.edu/pub/slackware/slackware64-14.1/';
+
+# # ___FIXME___ wrap these in IF clauses
+# let's ssh to the box, set up slackpkg, then get Perl and python installed.
+print "running: ssh $host 'echo $mirror{$platformbits} > /etc/slackpkg/mirrors'", "\n\n";
+system "ssh $host 'echo $mirror{$platformbits} > /etc/slackpkg/mirrors'";
+print "running: ssh $host 'echo Y | slackpkg update'", "\n\n";
+system "ssh $host 'echo Y | slackpkg update'";
+
+my $slackpkgcommand = "echo Y | slackpkg -dialog=off -default_answer=y -spinning=off -postinst=off install perl python";
+print "running: ssh $host '$slackpkgcommand'", "\n\n";
+system "ssh $host '$slackpkgcommand'";
+
+
+sub safechars {
+  my $string = shift;
+  $string =~ tr/a-zA-Z0-9\._-//dc;
+  return $string;
+}
